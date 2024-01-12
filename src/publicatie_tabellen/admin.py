aecb61b8baea3a46a57facdd38565dfdf9543e15
Manilde van der Oord <m.van.der.oord@amsterdam.nl>
2024-01-12 17:58:51 +0100

    added button to publish models to fill from database function

--- src/publicatie_tabellen/admin.py
+++ src/publicatie_tabellen/admin.py
@@ -1,4 +1,7 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.urls import path
+from django.http import HttpResponseRedirect
+from django.db import connection
 
 from publicatie_tabellen.models import (
     PublicationMeasure,
@@ -8,6 +11,8 @@ from publicatie_tabellen.models import (
 
 
 class NoAddDeleteChangePermission(admin.ModelAdmin):
+    change_list_template = "publicatie_tabellen/changelist.html"
+
     def has_add_permission(self, request) -> bool:
         return False
 
@@ -17,10 +22,22 @@ class NoAddDeleteChangePermission(admin.ModelAdmin):
     def has_change_permission(self, request, obj=None) -> bool:
         return False
 
+    def get_urls(self):
+        urls = super().get_urls()
+        my_urls = [
+            path('publish/', self.publish),
+        ]
+        return my_urls + urls
+
+    def publish(self, request):
+        get_message, state = publish_function(self.model)
+        self.message_user(request, get_message, state)
+        return HttpResponseRedirect("../")
+
 
 @admin.register(PublicationMeasure)
 class PublicationMeasureTypeAdmin(NoAddDeleteChangePermission):
-    list_display = ("name", "id")
+    list_display = ("name", "label", "id")
     ordering = ("id",)
 
 
@@ -34,3 +51,31 @@ class PublicationObservationAdmin(NoAddDeleteChangePermission):
 class PublicationStatisticAdmin(NoAddDeleteChangePermission):
     list_display = ("id",)
     ordering = ("id",)
+
+
+def publish_function(model):
+    """
+    runs the table function for publication:
+    truncate table first and than runs function/query
+    """
+
+    truncate = f""" TRUNCATE TABLE {model._meta.db_table} RESTART IDENTITY; """
+
+    match model._meta.model_name:
+        case "publicationmeasure":
+            raw_query = truncate + """ select public.publicatie_tabellen_publish_measures();"""
+
+        case "publicationstatstics":
+            raw_query = truncate + """ select  public.publicatie_tabellen_publish_statistics();"""
+
+    try:
+        print(raw_query)
+        with connection.cursor() as cursor:
+            cursor.execute(raw_query, {})
+        result = (f"All records for {model._meta.model_name} are imported", messages.SUCCESS)
+    except:
+        # error
+        result = (f"something went wrong", messages.ERROR)
+    
+    return result
+        
