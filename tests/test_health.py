ff17d3c444c36977e859d7a3f3f32ec05cfefb9f
manilde van der Oord <m.van.der.oord@amsterdam.nl>
2024-06-24 16:50:00 +0200

    changed util functions and add tests, and add per ha en m2 to unit.json

---
+++ tests/test_health.py
@@ -0,0 +1,20 @@
+import pytest
+from django.conf import settings
+from django.test.utils import override_settings
+
+from health.views import health
+
+
+class TestHealth():
+
+    @pytest.mark.django_db
+    def test_health_db(self,client):
+        """# check database"""
+        response = health(client)
+        assert response.status_code == 200
+
+    @override_settings(DEBUG=True)
+    def test_health_debug_true(self,client):
+        " debug = true"
+        response = health(client)
+        assert response.status_code == 500
