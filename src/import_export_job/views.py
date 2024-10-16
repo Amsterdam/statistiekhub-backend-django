268805dd12964ee20a5b81ad2af7ea88187ad308
manilde van der Oord <m.van.der.oord@amsterdam.nl>
2024-10-16 13:29:22 +0200

    add logging

--- src/import_export_job/views.py
+++ src/import_export_job/views.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 from django.conf import settings
@@ -5,18 +6,21 @@ from django.http import Http404, HttpResponse
 
 from main.utils_azure_storage import AzureStorage
 
+logger = logging.getLogger(__name__)
 
 def get_blob(request, blob_name):
+    logger.info(f"Requested blob_name: {blob_name}")
     if  hasattr(settings, 'STORAGE_AZURE'):
-        blob_service_client = AzureStorage.get_blob_service_client()
         try:
+            blob_service_client = AzureStorage.get_blob_service_client()
             container_name = settings.STORAGE_AZURE["default"]["OPTIONS"]["azure_container"]
             blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
             blob_data = blob_client.download_blob().readall()
 
             return HttpResponse(blob_data, content_type="text/html")
-        except Exception:
-            raise Http404(f"Bestand {blob_name} not found")
+        except Exception as e:
+            logger.error(f"Error retrieving blob from Azure: {e}")
+            raise Http404(f"Bestand {blob_name} niet gevonden")
     else:
         file_path = os.path.join(settings.MEDIA_ROOT, blob_name)
         if os.path.exists(file_path):
@@ -24,4 +28,4 @@ def get_blob(request, blob_name):
                 file_data = f.read()
             return HttpResponse(file_data, content_type="text/html")
         else:
-            raise Http404(f"File {blob_name} not found in media directory")        
+            raise Http404(f"File {blob_name} niet gevonden in media directory")        
