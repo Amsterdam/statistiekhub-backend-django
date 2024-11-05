96665d11628d6eae3d94092ac8af36ddf36350b0
manilde van der Oord <m.van.der.oord@amsterdam.nl>
2024-11-05 16:39:29 +0100

    add model updatedat for publications

--- src/publicatie_tabellen/models.py
+++ src/publicatie_tabellen/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+class PublicationUpdatedAt(models.Model):
+    updated_at = models.DateTimeField(auto_now=True)
+
 
 class PublicationMeasure(models.Model):
     name = models.CharField(unique=True, max_length=50)
