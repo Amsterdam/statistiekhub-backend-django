9b7dcc78da3ab1b0dd76c4cdad21bdc95f7aebc1
manilde van der Oord <m.van.der.oord@amsterdam.nl>
2024-11-05 16:40:18 +0100

    setup in pgdump management-command

--- src/publicatie_tabellen/management/commands/pgdump.py
+++ src/publicatie_tabellen/management/commands/pgdump.py
@@ -1,7 +1,10 @@
 import logging
 
+from django.db import transaction
 from django.core.management.base import BaseCommand
 
+from publicatie_tabellen.publication_main import PublishFunction
+from publicatie_tabellen.models import PublicationUpdatedAt
 from publicatie_tabellen.pgdump_to_storage import PgDumpToStorage
 
 logger = logging.getLogger(__name__)
@@ -12,16 +15,28 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         try:
-            app_names = ["publicatie_tabellen",]
-
-            dump = PgDumpToStorage()
-            dump.start_dump(app_names)
-            dump.upload_to_blob()
-            dump.remove_dump()
+            with transaction.atomic():
+                # calculate all publication-tables
+                updated_at = PublicationUpdatedAt.objects.last()
+                PublishFunction.run_all_publish_tables()
+                updated_at.save()
             
-            logger.info("Completed DB dump")
+                # pg_dump tables
+                app_names = ["publicatie_tabellen",]
+
+                dump = PgDumpToStorage()
+                try:                    
+                    dump.start_dump(app_names)
+                    dump.upload_to_blob()
+                    dump.remove_dump()
+                    
+                    logger.info("Completed DB dump")
+                except Exception as e:
+                    logger.exception(f"An exception occurred during pg_dump: {e}")
+                    raise  # transaction rollback
 
         except Exception as e:
             logger.exception(
                 f"An exception in dumping the publicationtables: {e}"
             )
+            raise
