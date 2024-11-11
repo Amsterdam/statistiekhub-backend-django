d4c827c62737e6a40498a22d228d285b3c7b7b87
manilde van der Oord <m.van.der.oord@amsterdam.nl>
2024-11-11 15:22:59 +0100

    resoved conversations

--- src/custom_db_backend/base.py
+++ src/custom_db_backend/base.py
@@ -1,3 +1,5 @@
+# Fix error: "connection already closed" 
+# How to deal with long running processes initiated via manage.py command?
 # see https://code.djangoproject.com/ticket/34914#comment:3 
 # implemented solution from: https://forum.djangoproject.com/t/django-db-utils-interfaceerror-connection-already-closed-when-updating-from-django-3-0-to-3-1/12708/21
 
@@ -5,9 +7,11 @@ import django.db
 from django.contrib.gis.db.backends.postgis.base import (
     DatabaseWrapper as BuiltinPostgresDatabaseWrapper,
 )
+from django.utils.asyncio import async_unsafe
 from psycopg2 import InterfaceError
 
 
+@async_unsafe
 class DatabaseWrapper(BuiltinPostgresDatabaseWrapper):
     def create_cursor(self, name=None):
         try:
--- src/import_export_job/tasks.py
+++ src/import_export_job/tasks.py
@@ -85,11 +85,7 @@ def _run_import_job(import_job, dry_run=True):
 
 def run_import_job(pk, dry_run=True):
     logger.info(f"Importing {pk} dry-run {dry_run}")
-    try:
-        import_job = models.ImportJob.objects.get(pk=pk)
-    except Exception as e:
-        logger.info(f"error op job {pk}: {e}")
-        return
+    import_job = models.ImportJob.objects.get(pk=pk)
 
     try:        
         _run_import_job(import_job, dry_run)
