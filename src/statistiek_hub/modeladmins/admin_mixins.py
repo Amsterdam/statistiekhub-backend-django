ef0c8c60ed9a53fb35a1dd30ad7fe91a2b6d1687
manilde van der Oord <m.van.der.oord@amsterdam.nl>
2025-07-04 12:26:04 +0200

    refactor filter on date field

--- src/statistiek_hub/modeladmins/admin_mixins.py
+++ src/statistiek_hub/modeladmins/admin_mixins.py
@@ -1,4 +1,4 @@
-from django.contrib.admin import ModelAdmin
+from django.contrib import admin
 from import_export.admin import ExportActionMixin, ImportMixin
 from import_export.formats import base_formats
 
@@ -39,3 +39,32 @@ class CheckPermissionUserMixin:
             theme_group = obj.measure.theme.group if hasattr(obj, 'measure') else obj.theme.group
             return theme_group in self._get_user_groups(request)
         return super().has_delete_permission(request)
+    
+
+class GenericDateFilter(admin.SimpleListFilter):
+    title = "date filter"
+    parameter_name = "date_field"
+
+    # The field to filter on, defaulting to "source_date"
+    filter_field = "source_date"
+
+    def lookups(self, request, model_admin):
+        """
+        Choices to propose all distinct values of the filter_field in the related model.
+        """
+        dates = list(
+            model_admin.model.objects.order_by(self.filter_field)
+            .values_list(self.filter_field, flat=True)
+            .distinct()
+        )
+        # Create a list of tuples for the filter dropdown
+        return [(str(date), str(date)) for date in dates]
+
+    def queryset(self, request, queryset):
+        """
+        Filter the queryset based on the selected value.
+        """
+        if self.value():  # If a value is selected, filter the queryset
+            filter_kwargs = {self.filter_field: self.value()}
+            return queryset.filter(**filter_kwargs)
+        return queryset        
