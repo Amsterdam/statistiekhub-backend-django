e07a90ae9d8ca601afdce13f3d412ed43f15de1c
manilde van der Oord <m.van.der.oord@amsterdam.nl>
2025-07-11 12:46:00 +0200

    new model Source

--- src/referentie_tabellen/models.py
+++ src/referentie_tabellen/models.py
@@ -40,3 +40,11 @@ class TemporalDimensionType(models.Model):
 
     def __str__(self):
         return f"{self.name}"
+
+class Source(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(unique=True, max_length=50)
+    name_long = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name}"
