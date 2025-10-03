d65fb6e0233e075cba0643a4ca220b7a64334b12
manilde van der Oord <m.van.der.oord@amsterdam.nl>
2025-10-03 18:11:02 +0200

    wildcard on publicationobservation search

--- src/publicatie_tabellen/admin.py
+++ src/publicatie_tabellen/admin.py
@@ -92,7 +92,7 @@ class PublicationObservationAdmin(NoAddDeleteChangePermission):
                 FROM {table_name}
                 WHERE measure LIKE %s
             """
-            raw_queryset = self.model.objects.raw(raw_query, [search_term.upper()])
+            raw_queryset = self.model.objects.raw(raw_query, [f"%{search_term.upper()}%"])
             ids = [obj.id for obj in raw_queryset]
             return self.model.objects.filter(id__in=ids)
 
