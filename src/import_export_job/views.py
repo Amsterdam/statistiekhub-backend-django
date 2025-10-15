774f3fd225fadbe0be6c67ed30046faa97ada89f
vanbuiten <david.vanbuiten@gmail.com>
2025-10-15 16:08:20 +0200

    refactor: refactored the get_blob view to use the default_storage and removed the azure storage singelton class
    The get_blob view can use the default_storage instead of the AzureStorage singleton class. Also the response if now a FileResponse that will stream the content to the browser

--- src/import_export_job/views.py
+++ src/import_export_job/views.py
@@ -1,35 +1,14 @@
 import logging
-import os
 
-from django.conf import settings
-from django.http import Http404, HttpResponse
-
-from main.utils_azure_storage import AzureStorage
+from django.core.files.storage import default_storage
+from django.http import FileResponse, Http404
 
 logger = logging.getLogger(__name__)
 
 
 def get_blob(request, blob_name):
-    if hasattr(settings, "STORAGE_AZURE"):
-        try:
-            blob_service_client = AzureStorage.get_blob_service_client()
-            container_name = settings.STORAGE_AZURE["default"]["OPTIONS"][
-                "azure_container"
-            ]
-            blob_client = blob_service_client.get_blob_client(
-                container=container_name, blob=blob_name
-            )
-            blob_data = blob_client.download_blob().readall()
+    if not default_storage.exists(blob_name):
+        raise Http404(f"Bestand {blob_name} niet gevonden")
 
-            return HttpResponse(blob_data, content_type="text/html")
-        except Exception as e:
-            logger.error(f"Error retrieving blob from Azure: {e}")
-            raise Http404(f"Bestand {blob_name} niet gevonden")
-    else:
-        file_path = os.path.join(settings.MEDIA_ROOT, blob_name)
-        if os.path.exists(file_path):
-            with open(file_path, "rb") as f:
-                file_data = f.read()
-            return HttpResponse(file_data, content_type="text/html")
-        else:
-            raise Http404(f"File {blob_name} niet gevonden in media directory")
+    file_obj = default_storage.open(blob_name, "rb")
+    return FileResponse(file_obj)
--- tests/test_main_utils.py
+++ tests/test_main_utils.py
@@ -1,5 +1,4 @@
 from main.utils_azure_queue import AzureQueue
-from main.utils_azure_storage import AzureStorage
 
 
 def test_singleton_queue():
@@ -8,14 +7,3 @@ def test_singleton_queue():
     queueclient_2 = AzureQueue()
     assert queueclient_1 is queueclient_2
     assert queueclient_1.get_queue_client() is queueclient_2.get_queue_client()
-
-
-def test_singleton_blobserviceclient():
-    # Singleton test
-    blobserviceclient_1 = AzureStorage()
-    blobserviceclient_2 = AzureStorage()
-    assert blobserviceclient_1 is blobserviceclient_2
-    assert (
-        blobserviceclient_1.get_blob_service_client()
-        is blobserviceclient_2.get_blob_service_client()
-    )
