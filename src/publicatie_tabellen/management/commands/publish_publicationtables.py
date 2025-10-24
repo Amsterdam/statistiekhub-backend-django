14a044a397ed4caa66a91144b0c877ec21b49630
manilde van der Oord <m.van.der.oord@amsterdam.nl>
2025-10-24 18:58:06 +0200

    make publish tabels possible  by management command

---
+++ src/publicatie_tabellen/management/commands/publish_publicationtables.py
@@ -0,0 +1,30 @@
+import logging
+
+from django.core.management.base import BaseCommand
+from django.db import transaction
+from opentelemetry import trace
+
+from publicatie_tabellen.publication_main import PublishFunction
+
+tracer = trace.get_tracer(__name__)
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    help = "Populates publication tabels"
+
+    def handle(self, *args, **options) -> None:
+        with tracer.start_as_current_span("publish") as span:
+            self._handle(*args, **options)
+
+    def _handle(self, *args, **options):
+        try:
+            with transaction.atomic():
+                publ = PublishFunction()
+                publ.run_all_publish_tables()
+
+        except Exception as e:
+            logger.exception(
+                f"An exception in command publish_publicationtables observations: {e}"
+            )
