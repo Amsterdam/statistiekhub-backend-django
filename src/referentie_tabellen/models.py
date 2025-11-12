fccba97bfd8af803db7bb6e34c1f730d785fda6b
vanbuiten <david.vanbuiten@gmail.com>
2025-11-12 15:41:22 +0100

    feat: a theme can only have a foreignkey to a group with a specific prefix

--- src/referentie_tabellen/models.py
+++ src/referentie_tabellen/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import Group
+from django.core.exceptions import ValidationError
 from django.db import models
 
 
@@ -18,10 +19,29 @@ class Theme(models.Model):
     name_uk = models.CharField(unique=True, max_length=50)
     abbreviation = models.CharField(unique=True, max_length=5)
     parent = models.ForeignKey("self", on_delete=models.SET_NULL, blank=True, null=True)
-    group = models.ForeignKey(Group, on_delete=models.SET_NULL, blank=True, null=True)
+
+    group_prefix = "theme_"
+    group = models.ForeignKey(
+        Group,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        limit_choices_to={"name__startswith": group_prefix},
+    )
 
     def __str__(self):
         return f"{self.name}"
+
+    def clean(self):
+        super().clean()
+        if self.group and not self.group.name.startswith(self.group_prefix):
+            raise ValidationError(
+                {"group": f'Group name must start with "{self.group_prefix}"'}
+            )
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        return super().save(*args, **kwargs)
 
 
 class SpatialDimensionType(models.Model):
